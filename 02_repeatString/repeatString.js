deed84895c1529603e7ddc9d100eab4328e68050
finnishboe <cdo21499@uniarts.fi>
2024-04-13 18:14:14 +0200

    Create a function that repeats a string
    Return a string repeated n number of times using
    the .repeat() method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string,number) {
+    if(number <0) return "ERROR";
+    return string.repeat(number)
 };
+
+console.log(repeatString("pula",10));
 
 // Do not edit below this line
 module.exports = repeatString;
