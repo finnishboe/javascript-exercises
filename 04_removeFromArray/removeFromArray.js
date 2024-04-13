01e9fc4ccec1be70b3c2e5d871f101d22580559e
finnishboe <cdo21499@uniarts.fi>
2024-04-13 18:38:58 +0200

    Create remove from array function
    The functions takes an indefinite amount of arguments.
    If the the arguments are part of the initial array
    the new array will filter them out.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,6 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numList, ...theArgs) {
+    newNumList = [];
+    return numList.filter(val => !theArgs.includes(val))
 };
 
 // Do not edit below this line
