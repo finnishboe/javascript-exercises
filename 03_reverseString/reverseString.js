e5458d7659a2bd56e24756cb16b77fa8ffa5ee3e
finnishboe <cdo21499@uniarts.fi>
2024-04-13 18:19:58 +0200

    Return reversed input string
    Create a function that iterates backwards through a string
    and attaches the letters to a new string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    new_string = ""
+    for (let i = string.length-1; i>= 0; i--){
+        new_string += string[i];
+    }
+    return new_string
 };
 
 // Do not edit below this line
