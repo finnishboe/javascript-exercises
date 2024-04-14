fff06a3664cf0bda998ae7e652a24812b3f25eb8
finnishboe <cdo21499@uniarts.fi>
2024-04-14 15:47:51 +0200

    Reinforce syntax knowledge with more exercises.
    Create function that calculates leap year using
    branches and function to convert temperatures.

---
+++ 06_leapYears/prog.py
@@ -0,0 +1,7 @@
+number = -100
+
+def wtf(number):
+    temp = (number-32) * 5/9
+    return round(temp,1)
+
+print(wtf(number))
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  let grades = (temp-32)*5/9;
+  return parseFloat(grades.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  let grades = 9/5*temp + 32
+  return parseFloat(grades.toFixed(1));
 };
 
 // Do not edit below this line
