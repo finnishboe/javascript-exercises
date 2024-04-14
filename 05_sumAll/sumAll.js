e73cd4cef585ab65a97b6593963f531d53c709db
finnishboe <cdo21499@uniarts.fi>
2024-04-14 15:20:55 +0200

    Practice using loops to sum each iteration
    Use a for loop and helper variable to sum each iteration.
    Write error handling conditional statements.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
-
+const sumAll = function(startNum,finishNum) {
+    let counter = 0;
+    if (Number.isInteger(startNum) && Number.isInteger(finishNum)){
+        if (startNum > 0 && finishNum > 0){
+            if (startNum > finishNum) {
+                for (let i = startNum; i >= finishNum; i--){
+                    counter += i;
+                }
+            } else {
+                for (let i = startNum; i <= finishNum; i++){
+                    counter += i;
+                }
+            }
+        } else{
+            return "ERROR"
+        }
+    } else {
+        return "ERROR"
+    }
+    return counter
 };
 
 // Do not edit below this line
